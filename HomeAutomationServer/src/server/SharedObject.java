6e6c3dc34277d1aec624c14dc75a094add5fdbfa
unknown <hyejinj810@gmail.com>
2020-05-06 17:46:56 +0900

    Build HomeAutomation Server, Web Server

---
+++ HomeAutomationServer/src/server/SharedObject.java
@@ -0,0 +1,35 @@
+package server;
+
+import java.util.LinkedList;
+
+/// ------------공유객체
+class SharedObject {
+	String TAG = "SharedObject";
+	Object monitor = new Object();
+	private LinkedList<String> dataList = new LinkedList<>();
+
+	public void put(String msg) {
+		synchronized (monitor) {
+			dataList.addLast(msg);
+			monitor.notify();
+		}
+	}
+
+	public String pop() {
+		String result = "";
+		synchronized (monitor) {
+			if (dataList.isEmpty()) {
+				try {
+					monitor.wait();
+					result = dataList.removeFirst();
+				} catch (InterruptedException e) {
+		
+				}
+			} else {
+				result = dataList.removeFirst();
+			}
+		}
+		return result;
+	}
+}
+//------------공유객체 끝
--- HomeAutomationWebServer/src/controller/WeatherController.java
+++ HomeAutomationWebServer/src/controller/WeatherController.java
@@ -18,41 +18,47 @@ import vo.WeatherVO;
 /**
  * Servlet implementation class WeatherController
  */
-@WebServlet("/WeatherController")
+@WebServlet("/getWeather")
 public class WeatherController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public WeatherController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	public WeatherController() {
+		super();
+
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		WeatherVO vo = new WeatherVO();
 		WeatherDAO dao = new WeatherDAO();
 		ArrayList<WeatherVO> result = dao.getWeather(vo);
+
 		response.setContentType("text/plain; charset=utf-8");
 		PrintWriter out = response.getWriter();
-		
+
 		ObjectMapper mapper = new ObjectMapper();
 		String jsonString = mapper.writeValueAsString(result);
 		out.println(jsonString);
 		out.flush();
 		out.close();
-		
+
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
