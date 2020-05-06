2065257005be1b03e5aa2536ed9b35480bd4c6a5
unknown <hyejinj810@gmail.com>
2020-05-06 10:21:00 +0900

    Update WeatherServer

--- HomeAutomationServer/src/controller/WeatherController.java
+++ HomeAutomationServer/src/controller/WeatherController.java
@@ -1,5 +1,60 @@
 package controller;
 
-public class WeatherController {
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import dao.WeatherDAO;
+import vo.WeatherVO;
+
+/**
+ * Servlet implementation class WeatherController
+ */
+@WebServlet("/WeatherController")
+public class WeatherController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public WeatherController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		WeatherVO vo = new WeatherVO();
+		WeatherDAO dao = new WeatherDAO();
+		ArrayList<WeatherVO> result = dao.getWeather(vo);
+		response.setContentType("text/plain; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		
+		ObjectMapper mapper = new ObjectMapper();
+		String jsonString = mapper.writeValueAsString(result);
+		out.println(jsonString);
+		out.flush();
+		out.close();
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
 
 }
